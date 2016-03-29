4f09d3fbcc206f499d64983c4f82e9ffc3ca362e
rhgksrua <rhgksrua@gmail.com>
2016-03-29 16:15:55 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var express = require('express');
+var morgan = require('morgan');
+
+var index = require('./routes/index');
+var api = require('./routes/api');
+
+var app = express();
+
+var port = process.env.PORT || 8000;
+
+app.use(morgan('combined'));
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('views', __dirname + '/templates');
+app.set('view engine', 'jade');
+
+app.use('/', index);
+app.use('/api', api);
+
+app.listen(port, function() {
+    console.log('listening on port: ' + port);
+})
